606db4909eb86fc26526379d8db296cacaa1efe5
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-05-02 11:34:06 +0300

    Code format

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import java.net.URI
 
 plugins {
-	id("org.jetbrains.dokka") version "1.8.10"
-	id("org.springframework.boot") version "3.0.5"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.20"
-	kotlin("plugin.serialization") version "1.8.20"
-	kotlin("plugin.spring") version "1.8.20"
+    id("org.jetbrains.dokka") version "1.8.10"
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
 }
 
 group = "eu.europa.ec.euidw"
@@ -16,51 +16,51 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
-	mavenLocal()
-	maven{
-		name="NiscyEudiwPackages"
-		url= URI("https://maven.pkg.github.com/niscy-eudiw/presentation-exchange-kt")
+    mavenCentral()
+	maven {
+		name = "NiscyEudiwPackages"
+		url = URI("https://maven.pkg.github.com/niscy-eudiw/presentation-exchange-kt")
 		credentials {
 			username = System.getenv("GITHUB_ACTOR")
 			password = System.getenv("GITHUB_TOKEN")
 		}
 
 	}
+	mavenLocal()
 }
 
 val presentationExchangeVersion = "1.0-SNAPSHOT"
 val nimbusSdkVersion = "10.7.1"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("eu.europa.ec.euidw:presentation-exchange-kt:$presentationExchangeVersion")
-	implementation("com.nimbusds:oauth2-oidc-sdk:$nimbusSdkVersion")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("eu.europa.ec.euidw:presentation-exchange-kt:$presentationExchangeVersion")
+    implementation("com.nimbusds:oauth2-oidc-sdk:$nimbusSdkVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 
 springBoot {
-	buildInfo()
+    buildInfo()
 }
 
 tasks.named<BootBuildImage>("bootBuildImage") {
-	imageName.set("$group/${project.name}")
+    imageName.set("$group/${project.name}")
 }
