fec4bf25f794f5725bd646fece882a622b50e497
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2025-03-28 11:00:03 +0200

    Improve error message formatting.

--- src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/adapter/out/sdjwtvc/SdJwtVerificationExceptionExtensions.kt
+++ src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/adapter/out/sdjwtvc/SdJwtVerificationExceptionExtensions.kt
@@ -21,31 +21,42 @@ import eu.europa.ec.eudi.sdjwt.VerificationError
 import eu.europa.ec.eudi.sdjwt.vc.SdJwtVcVerificationError
 
 internal val SdJwtVerificationException.description: String
-    get() = when (val sdJwtError = reason) {
-        VerificationError.ParsingError -> "sd-jwt could not be parsed"
-        is VerificationError.InvalidJwt ->
-            "sd-jwt contains an invalid jwt: ${listOfNotNull(sdJwtError.message, sdJwtError.cause?.message).joinToString(separator = ", ")}"
-        is VerificationError.KeyBindingFailed -> when (sdJwtError.details) {
-            KeyBindingError.InvalidKeyBindingJwt -> "keybinding jwt is not valid"
-            KeyBindingError.MissingHolderPubKey -> "missing holder public key (cnf)"
-            KeyBindingError.MissingKeyBindingJwt -> "missing keybinding jwt"
-            KeyBindingError.UnexpectedKeyBindingJwt -> "keybinding jwt was not expected"
-        }
-        is VerificationError.InvalidDisclosures -> "sd-jwt contains invalid disclosures"
-        is VerificationError.UnsupportedHashingAlgorithm -> "sd-jwt contains an unsupported hash algorithm: ${sdJwtError.algorithm}"
-        VerificationError.NonUniqueDisclosures -> "sd-jwt contains non-unique disclosures"
-        VerificationError.NonUniqueDisclosureDigests -> "sd-jwt contains non-unique digests"
-        is VerificationError.MissingDigests -> "sd-jwt contains disclosures for non-existing digests"
-        is VerificationError.SdJwtVcError -> when (val sdJwtVcError = sdJwtError.error) {
-            is SdJwtVcVerificationError.IssuerKeyVerificationError.UnsupportedVerificationMethod ->
-                "sd-jwt vc requires ${sdJwtVcError.method}, but this verification method is not enable"
-            is SdJwtVcVerificationError.IssuerKeyVerificationError.IssuerMetadataResolutionFailure ->
-                "unable to resolve sd-jwt vc issuer metadata ${sdJwtVcError.cause?.message}"
-            is SdJwtVcVerificationError.IssuerKeyVerificationError.UntrustedIssuerCertificate ->
-                "sd-jwt vc issuer certificate is not trusted ${sdJwtVcError.reason}"
-            is SdJwtVcVerificationError.IssuerKeyVerificationError.DIDLookupFailure ->
-                "did lookup failed ${listOfNotNull(sdJwtVcError.message, sdJwtVcError.cause?.message).joinToString(separator = ", ")}"
-            SdJwtVcVerificationError.IssuerKeyVerificationError.CannotDetermineIssuerVerificationMethod ->
-                "cannot determine verification method for sd-jwt vc. missing 'x5c' header claim, issuer is not an https url or did"
+    get() {
+        fun joinNotNullToString(vararg values: String?): String = values.filterNotNull().joinToString(separator = ", ")
+        fun joinNotBlank(vararg values: String?): String = values.filterNot { it.isNullOrBlank() }.joinToString(", ")
+
+        return when (val sdJwtError = reason) {
+            VerificationError.ParsingError -> "sd-jwt could not be parsed"
+            is VerificationError.InvalidJwt ->
+                joinNotBlank("sd-jwt contains an invalid jwt", joinNotNullToString(sdJwtError.message, sdJwtError.cause?.message))
+
+            is VerificationError.KeyBindingFailed -> when (sdJwtError.details) {
+                KeyBindingError.InvalidKeyBindingJwt -> "keybinding jwt is not valid"
+                KeyBindingError.MissingHolderPubKey -> "missing holder public key (cnf)"
+                KeyBindingError.MissingKeyBindingJwt -> "missing keybinding jwt"
+                KeyBindingError.UnexpectedKeyBindingJwt -> "keybinding jwt was not expected"
+            }
+
+            is VerificationError.InvalidDisclosures -> "sd-jwt contains invalid disclosures"
+            is VerificationError.UnsupportedHashingAlgorithm -> "sd-jwt contains an unsupported hash algorithm: ${sdJwtError.algorithm}"
+            VerificationError.NonUniqueDisclosures -> "sd-jwt contains non-unique disclosures"
+            VerificationError.NonUniqueDisclosureDigests -> "sd-jwt contains non-unique digests"
+            is VerificationError.MissingDigests -> "sd-jwt contains disclosures for non-existing digests"
+            is VerificationError.SdJwtVcError -> when (val sdJwtVcError = sdJwtError.error) {
+                is SdJwtVcVerificationError.IssuerKeyVerificationError.UnsupportedVerificationMethod ->
+                    "sd-jwt vc requires ${sdJwtVcError.method}, but this verification method is not enabled"
+
+                is SdJwtVcVerificationError.IssuerKeyVerificationError.IssuerMetadataResolutionFailure ->
+                    joinNotBlank("unable to resolve sd-jwt vc issuer metadata", sdJwtVcError.cause?.message)
+
+                is SdJwtVcVerificationError.IssuerKeyVerificationError.UntrustedIssuerCertificate ->
+                    joinNotBlank("sd-jwt vc issuer certificate is not trusted", sdJwtVcError.reason)
+
+                is SdJwtVcVerificationError.IssuerKeyVerificationError.DIDLookupFailure ->
+                    joinNotBlank("did lookup failed", joinNotNullToString(sdJwtVcError.message, sdJwtVcError.cause?.message))
+
+                SdJwtVcVerificationError.IssuerKeyVerificationError.CannotDetermineIssuerVerificationMethod ->
+                    "cannot determine verification method for sd-jwt vc. missing 'x5c' header claim, issuer is not an https url or did"
+            }
         }
     }
