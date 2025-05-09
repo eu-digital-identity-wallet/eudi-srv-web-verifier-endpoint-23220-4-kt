ccde891f4b8b47b18945b28ca9337eb4780eb363
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2025-05-09 17:35:04 +0300

    Add GenerateQrCode impl
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

---
+++ src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/adapter/out/qrcode/GenerateQrCodeFromData.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.verifier.endpoint.adapter.out.qrcode
+
+import eu.europa.ec.eudi.verifier.endpoint.port.out.qrcode.Dimensions
+import eu.europa.ec.eudi.verifier.endpoint.port.out.qrcode.GenerateQrCode
+import eu.europa.ec.eudi.verifier.endpoint.port.out.qrcode.PNGImage
+import eu.europa.ec.eudi.verifier.endpoint.port.out.qrcode.Pixels.Companion.toInt
+import qrcode.QRCode
+import qrcode.color.Colors
+import qrcode.raw.ErrorCorrectionLevel
+
+object GenerateQrCodeFromData : GenerateQrCode {
+    override suspend operator fun invoke(data: String, size: Dimensions): Result<PNGImage> {
+        return kotlin.runCatching {
+            QRCode
+                .ofSquares()
+                .withColor(Colors.BLACK)
+                .withSize(size.width.toInt())
+                .withErrorCorrectionLevel(ErrorCorrectionLevel.MEDIUM)
+                .build(data)
+                .renderToBytes()
+        }
+    }
+}
