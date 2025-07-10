6f60de6b9379d9ac9f99085c853dc116dd3a7086
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2025-07-10 16:19:41 +0300

    Cleanup Api from comments
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

--- src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/adapter/input/web/VerifierApi.kt
+++ src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/adapter/input/web/VerifierApi.kt
@@ -32,7 +32,6 @@ import kotlin.jvm.optionals.getOrNull
 internal class VerifierApi(
     private val initTransaction: InitTransaction,
     private val getWalletResponse: GetWalletResponse,
-//    private val getPresentationEvents: GetPresentationEvents,
     private val getClientMetadata: GetClientMetadata,
 ) {
 
@@ -45,7 +44,6 @@ internal class VerifierApi(
             ::handleInitTransaction,
         )
         GET(WALLET_RESPONSE_PATH, accept(APPLICATION_JSON), this@VerifierApi::handleGetWalletResponse)
-//        GET(EVENTS_RESPONSE_PATH, accept(APPLICATION_JSON), this@VerifierApi::handleGetPresentationEvents)
         GET(CLIENT_METADATA_PATH, accept(APPLICATION_JSON), this@VerifierApi::handleGetClientMetadata)
     }
 
@@ -95,23 +93,6 @@ internal class VerifierApi(
             is QueryResponse.Found -> found(result.value)
         }
     }
-
-    /**
-     * Handles a request placed by verifier, input order to obtain
-     * presentation logs
-     */
-//    private suspend fun handleGetPresentationEvents(req: ServerRequest): ServerResponse {
-// //        suspend fun found(events: PresentationEventsTO) = ok().json().bodyValueAndAwait(events)
-//
-//        val transactionId = req.transactionId()
-//
-//        logger.info("Handling Get PresentationEvents for tx ${transactionId.value}")
-//        return when (val result = getPresentationEvents(transactionId)) {
-//            is QueryResponse.NotFound -> notFound().buildAndAwait()
-//            is QueryResponse.InvalidState -> badRequest().buildAndAwait()
-//            is QueryResponse.Found -> found(result.value)
-//        }
-//    }
 
     private suspend fun handleGetClientMetadata(req: ServerRequest): ServerResponse =
         ok().json()
